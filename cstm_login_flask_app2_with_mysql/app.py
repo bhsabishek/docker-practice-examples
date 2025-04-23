a925e9da73e8b6cfe9c370f4ea664dd680643b12
bhsabishek <abishekbhs@gmail.com>
2025-04-23 18:29:43 +0530

    added code for all the files required to run the application

---
+++ cstm_login_flask_app2_with_mysql/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+import mysql.connector
+
+app = Flask(__name__)
+
+# MySQL Configuration
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="your_password",
+    database="user_db"
+)
+cursor = db.cursor()
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    query = "SELECT * FROM users WHERE username=%s AND password=%s"
+    cursor.execute(query, (username, password))
+    result = cursor.fetchone()
+
+    if result:
+        return render_template("welcome.html", username=username)
+    else:
+        return "Invalid credentials"
+
+if __name__ == '__main__':
+    app.run(debug=True)
